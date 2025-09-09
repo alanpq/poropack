55942532464e175809d49146f36097b6c7350ac2
Alan Panayotov <alanpanayotov@gmail.com>
2025-09-09 23:45:27 +0100

    fix (brex): deny warnings

--- brex/src/lib.rs
+++ brex/src/lib.rs
@@ -1,5 +1,6 @@
 //! Crate for encoding & decoding brex strings, an encoding dedicated to representing repetitive League of Legends game file names succinctly.
 #![deny(missing_docs)]
+#![deny(warnings)]
 
 mod decode;
 mod models;
