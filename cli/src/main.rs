62699ebad62c621f4d5973462b3f2937f2bc6cd1
Alan Panayotov <alanpanayotov@gmail.com>
2025-09-09 20:00:36 +0100

    feat (cli): add brex subcommand

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,12 @@
 use std::{
     collections::BTreeMap,
     fs::File,
-    io::{BufRead as _, BufReader, BufWriter, Write},
+    io::{BufRead as _, BufReader, BufWriter, Read, Write, stdin},
     path::PathBuf,
 };
 
 use clap::{Parser, Subcommand};
+use clap_stdin::FileOrStdin;
 use miette::IntoDiagnostic;
 
 use poro_hash::{Hasher, Hashtable, WadHasher, fst};
@@ -18,51 +19,85 @@ pub struct Cli {
 
 #[derive(Subcommand, Clone)]
 pub enum Commands {
+    #[command(subcommand)]
+    Brex(BrexCommand),
+    #[command(subcommand)]
+    Hash(HashCommand),
+}
+
+#[derive(Subcommand, Clone)]
+pub enum HashCommand {
     Compress { input: PathBuf, output: PathBuf },
     Decompress { input: PathBuf, output: PathBuf },
+}
+#[derive(Subcommand, Clone)]
+pub enum BrexCommand {
+    Encode {
+        input: FileOrStdin,
+        #[arg(short, long)]
+        output: Option<PathBuf>,
+    },
+    Decode {
+        input: FileOrStdin,
+        #[arg(short, long)]
+        output: Option<PathBuf>,
+    },
 }
 
 fn main() -> miette::Result<()> {
     let args = Cli::parse();
 
     match args.command {
-        Commands::Compress { input, output } => {
-            let file = BufReader::new(File::open(&input).into_diagnostic()?);
-
-            println!("Reading {input:?}...");
-            let mut entries = BTreeMap::new();
-            for line in file.lines() {
-                let line = line.into_diagnostic()?;
-                let (hash, value) = line.split_once(' ').unwrap();
-                let hash = u64::from_str_radix(hash, 16).into_diagnostic()?;
-                entries.insert(hash, value.trim().to_string());
+        Commands::Brex(command) => match command {
+            BrexCommand::Encode { input, output } => {
+                let mut lines = BufReader::new(input.into_reader().into_diagnostic()?).lines();
+                while let Some(Ok(line)) = lines.next() {
+                    eprintln!("{line}");
+                    let encoded = brex::encode(line.trim()).into_diagnostic()?;
+                    println!("{encoded}");
+                }
             }
+            BrexCommand::Decode { input, output } => todo!(),
+        },
+        Commands::Hash(command) => match command {
+            HashCommand::Compress { input, output } => {
+                let file = BufReader::new(File::open(&input).into_diagnostic()?);
 
-            let table = Hashtable::<WadHasher>::from(entries);
+                println!("Reading {input:?}...");
+                let mut entries = BTreeMap::new();
+                for line in file.lines() {
+                    let line = line.into_diagnostic()?;
+                    let (hash, value) = line.split_once(' ').unwrap();
+                    let hash = u64::from_str_radix(hash, 16).into_diagnostic()?;
+                    entries.insert(hash, value.trim().to_string());
+                }
 
-            println!("Compressing {} entries...", table.hashes.len());
-            let trie: fst::Set<Vec<u8>> = table.into();
-            let mut output = BufWriter::new(File::create(&output).into_diagnostic()?);
-            println!("Writing to {output:?}...");
+                let table = Hashtable::<WadHasher>::from(entries);
 
-            output
-                .write_all(trie.into_fst().as_bytes())
-                .into_diagnostic()?;
-        }
-        Commands::Decompress { input, output } => {
-            println!("Decompressing {input:?}...");
-            let set = fst::Set::new(std::fs::read(&input).unwrap()).unwrap();
-            let table = Hashtable::<WadHasher>::try_from(set).into_diagnostic()?;
+                println!("Compressing {} entries...", table.hashes.len());
+                let trie: fst::Set<Vec<u8>> = table.into();
+                let mut output = BufWriter::new(File::create(&output).into_diagnostic()?);
+                println!("Writing to {output:?}...");
 
-            println!("table w/ {} entries decompressed.", table.hashes.len());
-
-            let mut output = BufWriter::new(std::fs::File::create(&output).into_diagnostic()?);
-            let mut entries = table.hashes.into_iter().collect::<Vec<_>>();
-            entries.sort_unstable_by(|a, b| a.1.cmp(&b.1));
-            for (hash, value) in entries {
-                writeln!(output, "{hash:0>16x} {value}").into_diagnostic()?;
+                output
+                    .write_all(trie.into_fst().as_bytes())
+                    .into_diagnostic()?;
             }
-        }
+            HashCommand::Decompress { input, output } => {
+                println!("Decompressing {input:?}...");
+                let set = fst::Set::new(std::fs::read(&input).unwrap()).unwrap();
+                let table = Hashtable::<WadHasher>::try_from(set).into_diagnostic()?;
+
+                println!("table w/ {} entries decompressed.", table.hashes.len());
+
+                let mut output = BufWriter::new(std::fs::File::create(&output).into_diagnostic()?);
+                let mut entries = table.hashes.into_iter().collect::<Vec<_>>();
+                entries.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+                for (hash, value) in entries {
+                    writeln!(output, "{hash:0>16x} {value}").into_diagnostic()?;
+                }
+            }
+        },
     }
     Ok(())
 }
