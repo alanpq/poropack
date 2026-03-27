554abeafb84e1b992e3d6e62454fa206ada4780c
Alan Panayotov <alanpanayotov@gmail.com>
2026-03-27 19:28:03 +0000

    fix(cli): distinguish bin/wad hashes

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,7 +9,7 @@ use clap::{Parser, Subcommand};
 use clap_stdin::FileOrStdin;
 use miette::IntoDiagnostic;
 
-use poro_hash::{Hashtable, WadHash, fst};
+use poro_hash::{BinHash, FromStrRadix as _, Hashtable, WadHash, fst};
 
 #[derive(Parser)]
 pub struct Cli {
@@ -27,9 +27,24 @@ pub enum Commands {
 
 #[derive(Subcommand, Clone)]
 pub enum HashCommand {
-    Compress { input: PathBuf, output: PathBuf },
-    Decompress { input: PathBuf, output: PathBuf },
+    Compress {
+        kind: HashKind,
+        input: PathBuf,
+        output: PathBuf,
+    },
+    Decompress {
+        kind: HashKind,
+        input: PathBuf,
+        output: PathBuf,
+    },
 }
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, clap::ValueEnum)]
+pub enum HashKind {
+    Bin,
+    Wad,
+}
+
 #[derive(Subcommand, Clone)]
 pub enum BrexCommand {
     Encode {
@@ -79,22 +94,41 @@ fn main() -> miette::Result<()> {
             }
         },
         Commands::Hash(command) => match command {
-            HashCommand::Compress { input, output } => {
+            HashCommand::Compress {
+                kind,
+                input,
+                output,
+            } => {
                 let file = BufReader::new(File::open(&input).into_diagnostic()?);
 
                 println!("Reading {input:?}...");
-                let mut entries = BTreeMap::new();
-                for line in file.lines() {
-                    let line = line.into_diagnostic()?;
-                    let (hash, value) = line.split_once(' ').unwrap();
-                    let hash = u64::from_str_radix(hash, 16).into_diagnostic()?;
-                    entries.insert(WadHash(hash), value.trim().to_string());
-                }
+                let trie: fst::Set<Vec<u8>> = match kind {
+                    HashKind::Bin => {
+                        let mut entries = BTreeMap::new();
+                        for line in file.lines() {
+                            let line = line.into_diagnostic()?;
+                            let (hash, value) = line.split_once(' ').unwrap();
+                            let hash = BinHash::from_str_radix(hash, 16).into_diagnostic()?;
+                            entries.insert(hash, value.trim().to_string());
+                        }
+                        let table = Hashtable::from(entries);
+                        println!("Compressing {} entries...", table.hashes.len());
+                        table.into()
+                    }
+                    HashKind::Wad => {
+                        let mut entries = BTreeMap::new();
+                        for line in file.lines() {
+                            let line = line.into_diagnostic()?;
+                            let (hash, value) = line.split_once(' ').unwrap();
+                            let hash = WadHash::from_str_radix(hash, 16).into_diagnostic()?;
+                            entries.insert(hash, value.trim().to_string());
+                        }
+                        let table = Hashtable::from(entries);
+                        println!("Compressing {} entries...", table.hashes.len());
+                        table.into()
+                    }
+                };
 
-                let table = Hashtable::from(entries);
-
-                println!("Compressing {} entries...", table.hashes.len());
-                let trie: fst::Set<Vec<u8>> = table.into();
                 if let Some(parent) = output.parent() {
                     std::fs::create_dir_all(parent).into_diagnostic()?;
                 }
@@ -105,18 +139,40 @@ fn main() -> miette::Result<()> {
                     .write_all(trie.into_fst().as_bytes())
                     .into_diagnostic()?;
             }
-            HashCommand::Decompress { input, output } => {
+            HashCommand::Decompress {
+                kind,
+                input,
+                output,
+            } => {
                 println!("Decompressing {input:?}...");
                 let set = fst::Set::new(std::fs::read(&input).unwrap()).unwrap();
-                let table = Hashtable::<WadHash>::from_fst(set).into_diagnostic()?;
+                match kind {
+                    HashKind::Wad => {
+                        let table = Hashtable::<WadHash>::from_fst(set).into_diagnostic()?;
 
-                println!("table w/ {} entries decompressed.", table.hashes.len());
+                        println!("table w/ {} entries decompressed.", table.hashes.len());
 
-                let mut output = BufWriter::new(std::fs::File::create(&output).into_diagnostic()?);
-                let mut entries = table.hashes.into_iter().collect::<Vec<_>>();
-                entries.sort_unstable_by(|a, b| a.1.cmp(&b.1));
-                for (hash, value) in entries {
-                    writeln!(output, "{hash} {value}").into_diagnostic()?;
+                        let mut output =
+                            BufWriter::new(std::fs::File::create(&output).into_diagnostic()?);
+                        let mut entries = table.hashes.into_iter().collect::<Vec<_>>();
+                        entries.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+                        for (hash, value) in entries {
+                            writeln!(output, "{hash} {value}").into_diagnostic()?;
+                        }
+                    }
+                    HashKind::Bin => {
+                        let table = Hashtable::<BinHash>::from_fst(set).into_diagnostic()?;
+
+                        println!("table w/ {} entries decompressed.", table.hashes.len());
+
+                        let mut output =
+                            BufWriter::new(std::fs::File::create(&output).into_diagnostic()?);
+                        let mut entries = table.hashes.into_iter().collect::<Vec<_>>();
+                        entries.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+                        for (hash, value) in entries {
+                            writeln!(output, "{hash} {value}").into_diagnostic()?;
+                        }
+                    }
                 }
             }
         },
